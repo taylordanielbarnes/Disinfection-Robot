fa07c9214129dac4ab2c680d5ed17ee1b36de7b9
taylordanielbarnes <44959880+taylordanielbarnes@users.noreply.github.com>
2021-04-09 18:20:16 -0400

    add comments

--- uvc/src/server.cpp
+++ uvc/src/server.cpp
@@ -4,38 +4,59 @@
 
 #include <wiringPi.h>
 
+/*
+ *  send data to GPIO to control lamps
+ *
+ *  Service type: set_lamp.srv
+ *  Request: bool active
+ *  Response: bool status
+ *
+ *  if request is true, turn on lamps
+ *  if request is false, turn off lamps
+ */
 bool SetLamp(uvc::set_lamp::Request &req, uvc::set_lamp::Response &res)
 {
     if (req.active) {
+        //print status to console
         ROS_INFO("ON");
 
+        //turn lamps on via GPIO
         digitalWrite(8, 1);
 
+        //return status
         res.status = true;
     }
     else {
+        //print status to console
         ROS_INFO("OFF");
 
+        //turn lamps off via GPIO
         digitalWrite(8, 0);
 
+        //return status
         res.status = false;
     }
 
     return true;
 }
 
+
+
+/*
+ *  Establish Server
+ */
 int main(int argc, char **argv)
 {
     wiringPiSetup();			// Setup the library
     
-    pinMode(8, OUTPUT);		// Configure GPIO0 as an output
+    pinMode(8, OUTPUT);		// Configure GPIO8 as an output
 
+    //server initialization
     ros::init(argc, argv, "uvc_server");
-
     ros::NodeHandle node_handle;
-
     ros::ServiceServer service = node_handle.advertiseService("set_lamp", SetLamp);
 
+    //loop
     while (ros::ok()) {
         ros::spin();
     }
