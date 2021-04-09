f4b5483e3a9a99cad8da94b0cef99ada0170f92a
taylordanielbarnes <44959880+taylordanielbarnes@users.noreply.github.com>
2021-04-09 18:27:41 -0400

    more comments

--- uvc/src/client.cpp
+++ uvc/src/client.cpp
@@ -5,37 +5,42 @@
 #include <thread>
 #include <chrono>
 
+/*
+ *  Client node to demonstrate set_lamp service
+ *
+ *  Establish client node
+ *  Generate request to turn lamps off, then on, then off again
+ */
 int main(int argc, char **argv)
 {
+    //initialize client
     ros::init(argc, argv, "uvc_client");
-   
     ros::NodeHandle node_handle;
+    ros::ServiceClient client = node_handle.serviceClient<uvc::set_lamp>("set_lamp"); //change name to something unique?
 
-    ros::ServiceClient client = node_handle.serviceClient<uvc::set_lamp>("set_lamp");
-
+    //declare service
     uvc::set_lamp srv;
-
-    srv.request.active = false;
-
+    
+    //generate request to turn lamps off
+    srv.request.active = false; //set srv args
     client.call(srv);
+    ROS_INFO("OFF"); //print status to console
 
-    ROS_INFO("OFF");
-
+    //wait 10 seconds
     std::this_thread::sleep_for(std::chrono::seconds(10));
 
-    srv.request.active = true;
-    
+    //now generate request to turn lamps on
+    srv.request.active = true; //set srv args
     client.call(srv);
-    
-    ROS_INFO("ON");
+    ROS_INFO("ON"); //print status to console
 
+    //wait 10 seconds
     std::this_thread::sleep_for(std::chrono::seconds(10));
 
-    srv.request.active = false;
-
+    //turn lamps off again
+    srv.request.active = false; //set srv args
     client.call(srv);
-
-    ROS_INFO("OFF");
+    ROS_INFO("OFF"); //print status to console
 
     return 0;
 }
